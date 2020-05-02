08d72409135246660cff62c7f1f3cb8ba6f3de04
unknown <Nelson Oba>
2020-05-02 21:14:23 +0100

    verification changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8000/api',
- 
+  // baseUrl: 'http://localhost:8000/api',
+  baseUrl: 'http://bridgepay-env.eba-q3qfddpt.eu-west-2.elasticbeanstalk.com/api',
   imageUrl: 'https://bridge-pay.s3.eu-west-2.amazonaws.com/',
-  publishableKeyStripe: '',
-  paypalClientId: ''
+  publishableKeyStripe: 'pk_test_qkCQMaN7xcuxD4z2rZvZIRDR',
+  paypalClientId: 'ARDv4XhSU17jPAYNwmBf8VaYnoOxWj49eMT1bGFI6zdnFuAYLiZ8OOMVDJTy2zatdzZZv9MtHyB_ifT1'
 };
 
 /*
